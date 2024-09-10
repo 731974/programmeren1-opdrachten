fb775de6a779f01ced074f6c63f096bb8fc28b90
Mathijs <92179923+ThijsKlein@users.noreply.github.com>
2024-09-10 19:07:40 +0200

    Update
    Week 2 challenges (2/2)

---
+++ Week2/Assignment 12/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Assignment_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Program program = new Program();
+            program.Start();
+        
+        }
+        void Start()
+        {
+
+            Console.Write("Enter the first side: ");
+            int firstSide = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second side: ");
+            int secondSide = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the third side: ");
+            int thirdSide = int.Parse(Console.ReadLine());
+
+            DetermineTriangleType(firstSide, secondSide, thirdSide);
+
+        }
+
+        void DetermineTriangleType(int side1, int side2, int side3)
+        {
+
+            //Equilateral
+
+            if (side1 == side2 && side1 == side3 && side2 == side3)
+            {
+                Console.WriteLine("The triangle is Equilateral.");
+            }
+            //Isosceles
+            if ((side1 == side2 && side1 != side3) ||
+               (side1 == side3 && side1 != side2) ||
+               (side2 == side3 && side2 != side1))
+            {
+                Console.WriteLine("The triangle is Isosceles.");
+            }
+            //Scalene
+            else if (side1 != side2 && side1 != side3 && side2 != side3)
+            {
+                Console.WriteLine("The triangle is Scalene.");
+
+            }
+        }
+    }
+}
---
+++ Week2/Assignment 13/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Assignment_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Program program = new Program();
+            program.Start();
+
+        }
+        void Start()
+        {
+
+            Console.Write("Enter your balance: ");
+            int balance = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the amount to withdraw: ");
+            int amount = int.Parse(Console.ReadLine());
+
+            if(amount % 10 != 0) 
+            {
+                Console.WriteLine("Enter a correct amount, only tens are allowed.");
+
+                return;
+            }
+
+            bool isValidWithdrawal = IsValidWithdrawal(balance, amount);
+
+            if (isValidWithdrawal) {
+
+                Console.WriteLine(@$"Withdrawal succesfull 
+New balance: {balance - amount}");
+            } else
+            {
+                Console.WriteLine(@$"Withdrawal unsuccesfull.");
+            }
+        }
+
+        public bool IsValidWithdrawal(int balance, int amount)
+        {
+
+            if (balance >= amount)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+    }
+}
